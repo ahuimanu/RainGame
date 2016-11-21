1bfeb787857c3bd317fa9687d3c2e52196cd11dd
Jeffry Babb <ahuimanu@gmail.com>
2016-11-20 22:01:27 -0600

    Initial commit

---
+++ src/net/babbster/net/Game.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jeff on 11/20/2016.
+ */
+
+package net.babbster.net;
+
+public class Game {
+    public static int width = 300;
+    public static int height = width / 16 * 9;
+    public static int scale = 3;
+}
