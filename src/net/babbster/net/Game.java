51f8efc39858d8937f23906b96d37b16f7ba8ecb
Jeffry Babb <ahuimanu@gmail.com>
2016-11-20 22:18:03 -0600

    TheCherno Episode 3

--- src/net/babbster/net/Game.java
+++ src/net/babbster/net/Game.java
@@ -10,8 +10,10 @@ public class Game implements Runnable{
     public static int scale = 3;
 
     private Thread thread;
+    private boolean running = false;
 
     public synchronized void start(){
+        running = true;
         thread = new Thread(this, "Display");
         thread.start();
     }
@@ -26,6 +28,8 @@ public class Game implements Runnable{
 
     @Override
     public void run() {
-
+        while(running){
+            //game loop
+        }
     }
 }
